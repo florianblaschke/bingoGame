328442d8e24d685cb3d76e38786698bac597ce58
Florian Blaschke <florianblaschke@web.de>
2023-04-23 23:36:03 +0200

    version1

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cardArray = [
-    { name: "Ernst", img: "./images/ernst.jpg" },
-    { name: "Ernst", img: "./images/ernst.jpg" },
-    { name: "Ernst", img: "./images/ernst.jpg" },
-    { name: "Ernst", img: "./images/ernst.jpg" },
-    { name: "Ernst", img: "./images/ernst.jpg" },
-    { name: "Ernst", img: "./images/ernst.jpg" },
-    { name: "Ernst", img: "./images/ernst.jpg" },
-    { name: "Hello" },
-    { name: "bye" },
+    { name: "Ich habe eine Frage" },
+    { name: "Jemand erwähnt Kinder" },
+    { name: "Frage, die an Franz gehen sollte" },
+    { name: "Nicole stellt eine Frage" },
+    { name: "Alexandra stellt eine Frage" },
+    { name: "MÄCHTIG" },
+    { name: "Irgendwas mit GitHub" },
+    { name: "Jemand redet muted" },
+    { name: "Kann man meinen Bildschirm sehen?" },
+    { name: "Kind zu sehen" },
+    { name: "Ulrike liegt richtig" },
+    { name: "BROKKOLI" },
+    { name: "Frage nach Short-Cut" },
+    { name: "Entschuldigung, dass ich reinquatsche…" },
+    { name: "Erst erklärt etwas in Bildern" },
+    { name: "Katze von Ernst" },
   ];
 
   const grid = document.querySelector(".grid");
-  console.log(cardArray);
 
   //create a Board
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
-      var card = document.createElement("img");
-      card.setAttribute("src", "images/ernst.jpg");
-      // card.setAttribute("data-id", i);
-      grid.appendChild(card);
+      let fieldPiece = document.createElement("div");
+      let text = document.createElement("p");
+      // fieldPiece.setAttribute("class", "div");
+      // text.setAttribute("data-id");
+      grid.appendChild(fieldPiece);
+      fieldPiece.appendChild(text);
+      text.innerText = cardArray[1].name;
     }
-  }
   createBoard();
 });
